0d57edc09da7b2a9148b9efdb8245b5f9a869007
tulio <tulioov@gmail.com>
2022-04-01 16:03:17 -0300

    ajuste https

--- src/main/java/com/totem/TotemApplication.java
+++ src/main/java/com/totem/TotemApplication.java
@@ -1,19 +1,41 @@
 package com.totem;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TotemApplication extends SpringBootServletInitializer {
-	
+
+	@Value("${http.port}")
+	private int httpPort;
+
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TotemApplication.class);
-    }
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TotemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TotemApplication.class, args);
 	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
+	}
+
 }
